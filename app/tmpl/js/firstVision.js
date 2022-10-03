322efdbb156462c5f2723cf7c13fce66469ec68f
Артём Тюрин <61313696+rTiRe@users.noreply.github.com>
2022-10-03 20:30:46 +0300

    Add files via upload

--- app/tmpl/js/firstVision.js
+++ app/tmpl/js/firstVision.js
@@ -15,7 +15,7 @@ function trueSize() {
     containerWidth = container.offsetWidth;
     containerHeight = container.offsetHeight;
 
-    if(document.documentElement.clientWidth >= 768) {
+    if(document.documentElement.clientWidth >= 768 || (document.documentElement.clientWidth/document.documentElement.clientHeight <= 3/2)) {
         container.style.height = null;
         container.style.maxHeight = containerWidth/16*9 + "px";
         document.querySelector(".right").style.maxHeight = containerWidth/16*9 + 100 + "px";
